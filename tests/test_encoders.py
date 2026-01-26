4592294bc085764d49a352f8edd66bf18dfcc0d2
Naol Selemon <naol.selemon.scitech@gmail.com>
2026-01-26 17:20:30 +0300

    update(test-encoder): added normal encoder to test encoders

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -28,7 +28,7 @@ clear_gpu_memory()
 
 sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
 
-from models.encoders import DepthEncoder, SketchEncoder, MotionEncoder, StyleEncoder, MaskEncoder
+from models.encoders import DepthEncoder, SketchEncoder, MotionEncoder, StyleEncoder, MaskEncoder, NormalEncoder
 
 
 class EncoderTester:
@@ -160,7 +160,12 @@ class EncoderTester:
             expected_output_shape=(B, 256, T, H_out, W_out),
             test_backward=False
         )
-        
+        self.test_encoder(
+            NormalEncoder,
+            input_shape=(B, 3, T, H, W),
+            expected_output_shape=(B, 256, T, H_out, W_out),
+            test_backward=False
+        )
     
         print(f"\n{'='*60}")
         print(f"Testing StyleEncoder")
